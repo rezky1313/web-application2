3a21dfa8dff0d87256bb426a369d57081e265f0e
rezky1313 <rezkywahyudi1313@gmail.com>
2025-03-27 22:32:09 +0700

    Router using Chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/rezky1313/web-application2/pkg/config"
 	"github.com/rezky1313/web-application2/pkg/handlers"
 )
@@ -14,11 +14,20 @@ your routes right in your main folder/routine, so we gonna make separate files
 wich i will call routes.go
 */
 
-func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.HomeTemplate))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.AboutTemplate))
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.HomeTemplate))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.AboutTemplate))
+
+	//Router Using Chi
+
+	mux := chi.NewRouter()
+
+	mux.Get("/", handlers.Repo.HomeTemplate)
+	mux.Get("/about", handlers.Repo.AboutTemplate)
 
 	return mux
 
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.23.4
 
 require (
 	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 )
